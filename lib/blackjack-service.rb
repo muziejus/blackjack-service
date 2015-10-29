03dbd520f5e7ff00589e0681f48ea7690aaf59e8
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-10-28 23:18:04 -0400

    touch files I know I'll need

---
+++ lib/blackjack-service.rb
@@ -0,0 +1,11 @@
+require 'dotenv'
+require 'redis'
+
+require 'blackjack-service/game'
+require 'blackjack-service/deck'
+require 'blackjack-service/player'
+require 'blackjack-service/version'
+
+module BlackjackService
+
+end
